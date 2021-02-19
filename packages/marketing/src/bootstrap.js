1fc5188be1efcc954ec6be15a5064f4d4c5a5486
VodeniZeko <edvin.saletovic@pm.me>
2021-02-19 01:04:56 -0800

    setting up micro-front-end

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//Mount func to start the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+//if in dev mode and isolation , call mount immediately
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//if we are running thru container export mount function
+
+export { mount };
